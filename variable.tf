220596029294c23e68046fa0754dfbb0980519a7
mattshogi <mattshogi@users.noreply.github.com>
2019-09-01 19:36:35 -0500

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,19 @@
+
+variable "region" {
+  description = "AWS region for hosting our your network"
+  default = "us-east-1"
+}
+variable "public_key_path" {
+  description = "Enter the path to the SSH Public Key to add to AWS."
+  default = ""
+}
+variable "key_name" {
+  description = "Key name for SSHing into EC2"
+  default = ""
+}
+variable "amis" {
+  description = "Base AMI to launch the instances"
+  default = {
+  us-east-1 = "ami-713a6c1b"
+  }
+}
